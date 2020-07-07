a2a0d0809e0b7a68cc78d2ec1c1969e2644c71ad
Adam Ficke <aficke@gmail.com>
2020-07-07 16:09:50 -0400

    Table Formatting

---
+++ Table Formatting.R
@@ -0,0 +1,29 @@
+
+require(dplyr)
+require(Hmisc)
+require(kableExtra)
+
+row.names <- c("C()BRA", "New Custom","% Gini Improvement")
+col.names <- c("MD", "NCOIL")
+
+MD.fico <- "?"
+MD.CBRA <- 0.15962
+MD.new <- 0.17002
+
+NCOIL.CBRA <- 0.16466
+NCOIL.new <- 0.17463
+
+MD <- 
+  c(MD.CBRA, MD.new, 100 * (MD.new - MD.CBRA) / MD.CBRA)
+
+NCOIL <-
+  c(NCOIL.CBRA, NCOIL.new, 100 * (NCOIL.new - NCOIL.CBRA) / NCOIL.CBRA)
+
+disp <- data.frame(MD, NCOIL,row.names = row.names)
+
+knitr::kable(disp, align = "c",digits=3,) %>%
+  kable_styling(full_width = F) %>%
+  add_header_above(c(" " = 1, "Model Scorecard" = 2))
+
+
+
